658fad38a270d28cabb95ecaaf06510dc2a991fe
Leo <karta09104@gmail.com>
2019-04-11 22:13:57 +0800

    init

--- cresclab.js
+++ cresclab.js
@@ -1,7 +1,7 @@
 <script>
 alert("123");
 Shopify.Checkout.OrderStatus.addContentBox(
-    '<h2>{{order.order_id}}Pick-up in store</h2>',
-    '<p>{{order.order_number}}We are open everyday from 9am to 5pm.</p>'
+    '<h2>Pick-up in store</h2>',
+    '<p>{{ order.order_number }}We are open everyday from 9am to 5pm.</p>'
 )
 </script>
